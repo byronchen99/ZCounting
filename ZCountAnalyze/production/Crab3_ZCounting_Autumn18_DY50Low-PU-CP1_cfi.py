0683e887270ad56922944013555152b6093945f0
david <david.walter@desy.de>
2019-08-31 17:58:49 +0200

    add Autumn18 Low PU configs

---
+++ ZCountAnalyze/production/Crab3_ZCounting_Autumn18_DY50Low-PU-CP1_cfi.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'DY50_Summer16_V06'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/nfs/dust/cms/user/dwalter/CMSSW_9_4_13/src/ZCounting/ZCountAnalyze/test/ZCountAnalyze.py'
+config.JobType.allowUndistributedCMSSW = True
+#config.JobType.inputFiles = ['']
+
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/%s/ZCountAnalyze_V06/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.outputDatasetTag = 'DY50_Summer16_V06'
+
+config.Site.storageSite = 'T2_DE_DESY'
+#config.Site.blacklist = ['T3_US_Baylor','T2_US_Vanderbilt','T2_US_UCSD','T2_US_Florida' ]
