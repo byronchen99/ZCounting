a3f507bbee75c087c26462c6ac8609594b8d208b
david <david.walter@desy.de>
2019-09-18 11:16:28 +0200

    update crab production files

--- ZCountAnalyze/production/Crab3_ZCounting_Autumn18_DY50_cfi.py
+++ ZCountAnalyze/production/Crab3_ZCounting_Autumn18_DY50_cfi.py
@@ -7,11 +7,11 @@ config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/nfs/dust/cms/user/dwalter/CMSSW_9_4_13/src/ZCounting/ZCountAnalyze/test/ZCountAnalyze.py'
+config.JobType.psetName = '/nfs/dust/cms/user/dwalter/CMSSW_10_2_13/src/ZCounting/ZCountAnalyze/test/ZCountAnalyze.py'
 config.JobType.allowUndistributedCMSSW = True
 #config.JobType.inputFiles = ['']
 
-config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext2-v2/MINIAODSIM'
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 4
--- ZCountAnalyze/production/Crab3_ZCounting_Summer16_DY50_cfi.py
+++ ZCountAnalyze/production/Crab3_ZCounting_Summer16_DY50_cfi.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'DY50_Summer16_V06'
+config.General.requestName = 'DY50_Summ16_V07'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -11,13 +11,13 @@ config.JobType.psetName = '/nfs/dust/cms/user/dwalter/CMSSW_9_4_13/src/ZCounting
 config.JobType.allowUndistributedCMSSW = True
 #config.JobType.inputFiles = ['']
 
-config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext1-v2/MINIAODSIM'
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3_ext2-v2/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
-config.Data.outLFNDirBase = '/store/user/%s/ZCountAnalyze_V06/' % (getUsernameFromSiteDB())
+config.Data.unitsPerJob = 4
+config.Data.outLFNDirBase = '/store/user/%s/ZCountAnalyze_Summer16_V07/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'DY50_Summer16_V06'
+config.Data.outputDatasetTag = 'DY50_Summer16_V07'
 
 config.Site.storageSite = 'T2_DE_DESY'
 #config.Site.blacklist = ['T3_US_Baylor','T2_US_Vanderbilt','T2_US_UCSD','T2_US_Florida' ]
