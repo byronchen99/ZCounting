9b8cb1644f0dcbfdeb9a397f9f6566a0ef078af8
david <david.walter@desy.de>
2020-04-06 22:43:53 +0200

    add notebooks for altair plotting

---
+++ ZHarvester/python/utils.py
@@ -0,0 +1,50 @@
+import json, os
+
+def chart_to_js(_chart, _name, _data="data.csv"):
+    chart_json = _chart.to_dict()
+    # delete in line data
+    del chart_json["datasets"]
+    # add external data resource
+    chart_json["data"] = {"url": _data}
+    with open(_name+'.js','a') as file:
+        file.write("""(function(vegaEmbed) {
+    var spec = """)
+        json.dump(chart_json, file, indent=4)
+        file.write(""";
+    var embedOpt = {\"renderer\": \"svg\", \"mode\": \"vega-lite\"};
+    function showError(el, error){
+        el.innerHTML = ('<div class=\"error\" style=\"color:red;\">'
+        + '<p>JavaScript Error: ' + error.message + '</p>'
+        + \"<p>This usually means there's a typo in your chart specification. \"
+        + \"See the javascript console for the full traceback.</p>\"
+        + '</div>');
+        throw error;
+    }""")
+        file.write("const el = document.getElementById('vis');\n")
+        file.write("vegaEmbed(\"#vis\", spec, embedOpt).catch(error => showError(el, error));\n")
+        file.write("})(vegaEmbed);\n")
+
+def chart_to_html(_chart, _name, **kwargs):
+
+    with open(_name+'.js','w') as file:
+        file.write("""
+<!DOCTYPE html>
+<html>
+<head>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega@5"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega-lite@4.8.1"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega-embed@6"></script>
+</head>
+<body>
+  <div id="vis"></div>
+  <script type="text/javascript">
+    """)
+    chart_to_js(_chart, _name, **kwargs)
+    with open(_name+'.js','a') as file:
+        file.write("""
+  </script>
+</body>
+</html>
+""")
+
+    os.system(f"mv {_name}.js {_name}.html")
