fec413ecc7b89135a2f084868db24c74a61c2ccd
david <david.walter@desy.de>
2021-04-14 10:41:20 +0200

    add more trigger paths, also for 2016

--- ZCountAnalyze/test/ZCountAnalyze.py
+++ ZCountAnalyze/test/ZCountAnalyze.py
@@ -52,9 +52,16 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxE
 process.source = cms.Source("PoolSource",
     fileNames=cms.untracked.vstring(
         # '/store/mc/RunIIFall17MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/00000/CE864285-6D1C-E911-B09D-34E6D7BDDECE.root'
+        # '/store/mc/RunIIAutumn18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU0to75TuneCP5_102X_upgrade2018_realistic_v15-v1/00000/01C7EF7D-6860-7F41-A815-5CCC6856AB28.root'
         # '/store/mc/RunIIFall17MiniAODv2/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/20000/0075FEFF-D341-E811-AD4B-001E673D35A9.root'
         # '/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU0to75TuneCUETP8M1_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/50000/128C7A83-4F17-E711-A485-FA163E23B354.root'
-        '/store/mc/RunIISummer16MiniAODv3/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU28to62HcalNZSRAW_94X_mcRun2_asymptotic_v3-v1/250000/1A702941-9A6C-E911-A2BD-AC1F6BAB6860.root'
+        # '/store/mc/RunIISummer16MiniAODv3/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU28to62HcalNZSRAW_94X_mcRun2_asymptotic_v3-v1/250000/1A702941-9A6C-E911-A2BD-AC1F6BAB6860.root'
+
+        # UL 2016
+        # '/store/mc/RunIISummer20UL16MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU0to75_106X_mcRun2_asymptotic_v13-v2/230000/0ACCBC6C-6308-2D42-8D55-18F3522FE776.root'
+        # 'root://cmsxrootd.fnal.gov//store/mc/RunIISummer20UL16MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU0to75_106X_mcRun2_asymptotic_v13-v2/230000/10E0D710-F696-7248-9A7C-80B0F127C6B5.root'
+        # UL 2016 APV
+        '/store/mc/RunIISummer20UL16MiniAODAPV/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/FlatPU0to75_106X_mcRun2_asymptotic_preVFP_v8-v2/00000/0040C830-7251-BA46-BE12-86EE8CFD0907.root'
     )
 )
 
@@ -65,11 +72,6 @@ process.TFileService = cms.Service("TFileService", fileName=cms.string(outFileNa
 process.load("ZCounting.ZCountAnalyze.ZCounting_cfi")
 
 tasks = cms.Task()
-
-if options.year == 2016:
-    process.zcounting.muon_trigger_patterns=cms.vstring(
-        "HLT_IsoMu24_v*",
-        "HLT_IsoTkMu24_v*")
 
 # if no good Z candidate is found. E.g. if gamma is found instead
 if options.samplename == 'dy':
