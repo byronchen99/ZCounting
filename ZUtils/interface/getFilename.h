f2736bd7a1bc68e7795dd1685902bd6b78a79a85
david walter <david.walter2@student.kit.edu>
2022-07-14 07:46:29 +0200

    Add files with empty functions  to have a successful compilation. The actual function is needed to emulate the trigger in 2017H but is too heavy to upload to git. For this reasen it is kept lokally

---
+++ ZUtils/interface/getFilename.h
@@ -0,0 +1,7 @@
+#ifndef GETFILENAME_H_
+#define GETFILENAME_H_
+
+#include <string>
+const std::string getFilename(int runNumber, int lumisection);
+
+#endif
---
+++ ZUtils/src/getFilename.cc
@@ -0,0 +1,5 @@
+#include "ZCounting/ZUtils/interface/getFilename.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+const std::string getFilename(int runNumber, int lumisection){
+    return "None";
+}
