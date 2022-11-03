1e7af16f2ac6152fbce640bba435ac978cb12b85
Thales Menezes De Oliveira <tmenezes@cern.ch>
2022-11-03 16:43:43 +0100

    Muon fix

--- cronCopyDQMFiles.sh
+++ cronCopyDQMFiles.sh
@@ -4,16 +4,15 @@
 #Set Variables
 #############
 
-pass=''            # grid passwd
+pass='rainbowsix'            # grid passwd
 cmsswRel='/afs/cern.ch/user/t/tmenezes/work/private/CMSSW_12_4_2/src/'   # path to ZCounting dir.
-PD='/Muon/'
+#PD='/SingleMuon/'
 # After the RunID = 356426, SingleMuon -> Muon 
-#PD='/Muon/'
+PD='/Muon/'
 COREDIR='https://cmsweb.cern.ch/dqm/offline/data/browse/ROOT/OfflineData/Run2022/'
 PDDIR=$COREDIR$PD
 certif=/tmp/x509up_u81013     # you get one after the voms (grid proxy)
 targetDir=/eos/cms/store/group/comm_luminosity/ZCounting/2022/DQMFiles/
-
 
 ##################
 #Get List Of Files
@@ -30,7 +29,8 @@ rm -rf filelist.log
 # Create the log files with the files from the PDDIR (uses grid certs. and grid passwd)
 for rundir in `curl -k --cert $certif -X GET $PDDIR | awk 's=index($0,"/000") { print substr($0,s+1,10)}'` 
 do
-    curl -k --cert ~/.globus/usercert.pem --key ~/.globus/userkey.pem --pass $pass -X GET $PDDIR$rundir | awk  -v COREDIR="$COREDIR" -F '<tr><td>' '{ print COREDIR substr($2,60,89) } ' >> filelist.log
+    #curl -k --cert ~/.globus/usercert.pem --key ~/.globus/userkey.pem --pass $pass -X GET $PDDIR$rundir | awk  -v COREDIR="$COREDIR" -F '<tr><td>' '{ print COREDIR substr($2,60,89) } ' >> filelist.log    # SingleMuon
+    curl -k --cert ~/.globus/usercert.pem --key ~/.globus/userkey.pem --pass $pass -X GET $PDDIR$rundir | awk  -v COREDIR="$COREDIR" -F '<tr><td>' '{ print COREDIR substr($2,60,77) } ' >> filelist.log     # Muon
 done
 
 
