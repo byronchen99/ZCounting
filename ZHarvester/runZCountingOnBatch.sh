a2b00c7ff7533f15395fa193ffcc5c98e78ea533
david walter <david.walter2@student.kit.edu>
2023-02-10 18:37:56 +0100

    Work on submit script, does not run yet...

--- ZHarvester/runZCountingOnBatch.sh
+++ ZHarvester/runZCountingOnBatch.sh
@@ -6,6 +6,8 @@ workdir=$2/src/ZCounting/ZHarvester
 
 TOP="$PWD"
 
+echo "Prepare environment"
+
 cd $2
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd src
@@ -16,4 +18,6 @@ cd $TOP
 cp -r ${workdir}/Utils $TOP
 cp ${workdir}/ZCounting.py $TOP
 
-python3 ZCounting.py -b $runNum -e $(($runNum + 1)) --dirDQM $3 --byLsCSV $4 -o $5 --mcCorrections $6 --sigModels $7 --bkgModels $8 --ptCut $9 --etaCut ${10} --mass ${11} ${12} ${13} --LumiPerMeasurement ${14} --mode ${15}
+echo "run ZCounting.py"
+
+python3 ZCounting.py -b $runNum -e $(($runNum + 1)) --input $3 --byLsCSV $4 -o $5 --mcCorrections $6 --sigModels $7 --bkgModels $8 --ptCut $9 --etaCut ${10} --mass ${11} ${12} ${13} --LumiPerMeasurement ${14} --mode ${15}
